24ab882693cef1a70349726daed164389354ac13
elehmzzz <elehmzzz@gmail.com>
2026-07-09 20:22:44 -0600

    Ajustes en inicialización y comentarios del formulario
    Se modificó la inicialización del formulario en `FrmPerfilEmpleado.Designer.cs`, eliminando el uso explícito de `this` y ajustando las dimensiones del formulario de `800x450` a `942x493`. También se añadieron llamadas a `SuspendLayout()` y `ResumeLayout(false)` para optimizar el diseño.
    
    En `FrmPerfilEmpleado.resx`, se reorganizaron y ajustaron los comentarios del archivo XML para mejorar la legibilidad, eliminando líneas redundantes y espacios innecesarios. No se realizaron cambios funcionales en los datos.

--- Clases/FrmPerfilEmpleado.Designer.cs
+++ Clases/FrmPerfilEmpleado.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmPerfilEmpleado";
+            SuspendLayout();
+            // 
+            // FrmPerfilEmpleado
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(942, 493);
+            Name = "FrmPerfilEmpleado";
+            Text = "FrmPerfilEmpleado";
+            ResumeLayout(false);
         }
 
         #endregion
