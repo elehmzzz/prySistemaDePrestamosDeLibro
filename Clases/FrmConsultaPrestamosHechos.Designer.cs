74e1568814da8c335970651382c2bc365df4022e
alexaviteramirez-bit <alexaviteramirez@gmail.com>
2026-07-10 11:15:57 -0600

    agregando cambios de de Prestamos, tres pantallass

---
+++ Clases/FrmConsultaPrestamosHechos.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace prySistemaDePrestamosDeLibro.Clases
+{
+    partial class FrmConsultaPrestamosHechos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel2 = new Panel();
+            dateTimePicker1 = new DateTimePicker();
+            textBox9 = new TextBox();
+            label10 = new Label();
+            label9 = new Label();
+            textBox7 = new TextBox();
+            label7 = new Label();
+            textBox6 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            textBox5 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            panel1 = new Panel();
+            label5 = new Label();
+            label6 = new Label();
+            textBox3 = new TextBox();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            panel3 = new Panel();
+            labelRegistrodePrestamos = new Label();
+            textBox4 = new TextBox();
+            label8 = new Label();
+            label11 = new Label();
+            textBox8 = new TextBox();
+            button2 = new Button();
+            label12 = new Label();
+            textBox10 = new TextBox();
+            label13 = new Label();
+            textBox11 = new TextBox();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(236, 223, 204);
+            panel2.Controls.Add(textBox11);
+            panel2.Controls.Add(label13);
+            panel2.Controls.Add(textBox10);
+            panel2.Controls.Add(label12);
+            panel2.Controls.Add(textBox8);
+            panel2.Controls.Add(label11);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(textBox4);
+            panel2.Controls.Add(dateTimePicker1);
+            panel2.Controls.Add(textBox9);
+            panel2.Controls.Add(label10);
+            panel2.Controls.Add(label9);
+            panel2.Controls.Add(textBox7);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(textBox6);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(textBox5);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(panel1);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(panel3);
+            panel2.Controls.Add(button2);
+            panel2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            panel2.Location = new Point(-37, -9);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1349, 731);
+            panel2.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(380, 212);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(215, 34);
+            dateTimePicker1.TabIndex = 31;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(946, 214);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(215, 34);
+            textBox9.TabIndex = 29;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(662, 212);
+            label10.Name = "label10";
+            label10.Size = new Size(180, 28);
+            label10.TabIndex = 28;
+            label10.Text = "Fecha de Prestamo:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(662, 150);
+            label9.Name = "label9";
+            label9.Size = new Size(75, 28);
+            label9.TabIndex = 27;
+            label9.Text = "Estado:";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(380, 476);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(215, 34);
+            textBox7.TabIndex = 24;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(157, 479);
+            label7.Name = "label7";
+            label7.Size = new Size(58, 28);
+            label7.TabIndex = 23;
+            label7.Text = "ISBN:";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(380, 404);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(215, 34);
+            textBox6.TabIndex = 22;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(157, 407);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 28);
+            label4.TabIndex = 21;
+            label4.Text = "Autor:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(157, 339);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 28);
+            label3.TabIndex = 20;
+            label3.Text = "Libro:";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(380, 336);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(215, 34);
+            textBox5.TabIndex = 19;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(380, 271);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(215, 34);
+            textBox2.TabIndex = 18;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(157, 274);
+            label2.Name = "label2";
+            label2.Size = new Size(126, 28);
+            label2.TabIndex = 16;
+            label2.Text = "ID del Lector:";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(32, 41, 64);
+            panel1.Dock = DockStyle.Left;
+            panel1.Font = new Font("Segoe UI", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            panel1.ForeColor = SystemColors.Control;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(120, 731);
+            panel1.TabIndex = 14;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(29, 153);
+            label5.Name = "label5";
+            label5.Size = new Size(89, 28);
+            label5.TabIndex = 4;
+            label5.Text = "Nombre:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(157, 150);
+            label6.Name = "label6";
+            label6.Size = new Size(122, 28);
+            label6.TabIndex = 5;
+            label6.Text = "ID Prestamo:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(946, 144);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(215, 34);
+            textBox3.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(380, 147);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(215, 34);
+            textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(157, 212);
+            label1.Name = "label1";
+            label1.Size = new Size(180, 28);
+            label1.TabIndex = 10;
+            label1.Text = "Nombre del Lector:";
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(32, 41, 64);
+            panel3.Controls.Add(labelRegistrodePrestamos);
+            panel3.Font = new Font("Segoe UI", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            panel3.ForeColor = SystemColors.Control;
+            panel3.Location = new Point(5, 9);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1344, 87);
+            panel3.TabIndex = 15;
+            // 
+            // labelRegistrodePrestamos
+            // 
+            labelRegistrodePrestamos.AutoSize = true;
+            labelRegistrodePrestamos.BackColor = Color.FromArgb(32, 41, 64);
+            labelRegistrodePrestamos.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            labelRegistrodePrestamos.ForeColor = Color.White;
+            labelRegistrodePrestamos.Location = new Point(143, 23);
+            labelRegistrodePrestamos.Name = "labelRegistrodePrestamos";
+            labelRegistrodePrestamos.Size = new Size(405, 46);
+            labelRegistrodePrestamos.TabIndex = 3;
+            labelRegistrodePrestamos.Text = "Información del Prestamo";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(946, 271);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(215, 34);
+            textBox4.TabIndex = 32;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(662, 274);
+            label8.Name = "label8";
+            label8.Size = new Size(255, 28);
+            label8.TabIndex = 33;
+            label8.Text = "Fecha Limite de Devolucion:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(662, 339);
+            label11.Name = "label11";
+            label11.Size = new Size(140, 28);
+            label11.TabIndex = 34;
+            label11.Text = "Dias Restantes:";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(946, 336);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(215, 34);
+            textBox8.TabIndex = 35;
+            // 
+            // button2
+            // 
+            button2.AutoSize = true;
+            button2.BackColor = Color.FromArgb(32, 41, 64);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(1055, 597);
+            button2.Name = "button2";
+            button2.Size = new Size(184, 38);
+            button2.TabIndex = 30;
+            button2.Text = "Siguiente";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(662, 407);
+            label12.Name = "label12";
+            label12.Size = new Size(150, 28);
+            label12.TabIndex = 36;
+            label12.Text = "Dias de Retraso:";
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(946, 404);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(215, 34);
+            textBox10.TabIndex = 37;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(662, 479);
+            label13.Name = "label13";
+            label13.Size = new Size(156, 28);
+            label13.TabIndex = 38;
+            label13.Text = "Multa Generada:";
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(946, 476);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(215, 34);
+            textBox11.TabIndex = 39;
+            // 
+            // FrmConsultaPrestamoHechoVigente
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1274, 648);
+            Controls.Add(panel2);
+            Name = "FrmConsultaPrestamoHechoVigente";
+            Text = "FrmConsultaPrestamoHechoVigente";
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel2;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox9;
+        private Label label10;
+        private Label label9;
+        private TextBox textBox7;
+        private Label label7;
+        private TextBox textBox6;
+        private Label label4;
+        private Label label3;
+        private TextBox textBox5;
+        private TextBox textBox2;
+        private Label label2;
+        public Panel panel1;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox3;
+        private TextBox textBox1;
+        private Label label1;
+        public Panel panel3;
+        private Label labelRegistrodePrestamos;
+        private Label label8;
+        private TextBox textBox4;
+        private TextBox textBox11;
+        private Label label13;
+        private TextBox textBox10;
+        private Label label12;
+        private TextBox textBox8;
+        private Label label11;
+        private Button button2;
+    }
+}
