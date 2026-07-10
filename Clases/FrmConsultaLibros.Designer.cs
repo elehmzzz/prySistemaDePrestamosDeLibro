8cf5836a76ae7c88b01a3e3ff421f4244b08a59b
Hermelinda.Cruz <20251066@uthh.edu.mx>
2026-07-10 17:04:10 -0500

    se crearon los frmConsulta,frmNuevoLibro y FrmEditConsultaLibro

---
+++ Clases/FrmConsultaLibros.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace prySistemaDePrestamosDeLibro.Clases
+{
+    partial class FrmConsultaLibros
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            textBoxEDispo = new TextBox();
+            textBoxETotal = new TextBox();
+            textBoxCategoria = new TextBox();
+            textBoxISBN = new TextBox();
+            textBoxAutor = new TextBox();
+            textBoxTitulo = new TextBox();
+            textBoxid = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            editar = new Button();
+            salir = new Button();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            igualejemplar = new Label();
+            label1 = new Label();
+            panel2.SuspendLayout();
+            panel4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(32, 41, 64);
+            panel1.Location = new Point(1, 74);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(110, 391);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(236, 223, 204);
+            panel2.Controls.Add(label9);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(textBoxEDispo);
+            panel2.Controls.Add(textBoxETotal);
+            panel2.Controls.Add(textBoxCategoria);
+            panel2.Controls.Add(textBoxISBN);
+            panel2.Controls.Add(textBoxAutor);
+            panel2.Controls.Add(textBoxTitulo);
+            panel2.Controls.Add(textBoxid);
+            panel2.Controls.Add(dateTimePicker1);
+            panel2.Controls.Add(editar);
+            panel2.Controls.Add(salir);
+            panel2.Font = new Font("Segoe UI", 9F);
+            panel2.Location = new Point(110, 74);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(691, 375);
+            panel2.TabIndex = 1;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 10F);
+            label9.Location = new Point(389, 17);
+            label9.Name = "label9";
+            label9.Size = new Size(120, 19);
+            label9.TabIndex = 17;
+            label9.Text = "Fecha de Registro:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 10F);
+            label8.Location = new Point(34, 286);
+            label8.Name = "label8";
+            label8.Size = new Size(151, 19);
+            label8.TabIndex = 16;
+            label8.Text = "Ejemplares Disponibles:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 10F);
+            label7.Location = new Point(34, 244);
+            label7.Name = "label7";
+            label7.Size = new Size(123, 19);
+            label7.TabIndex = 15;
+            label7.Text = "Ejemplares Totales:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 10F);
+            label6.Location = new Point(34, 192);
+            label6.Name = "label6";
+            label6.Size = new Size(71, 19);
+            label6.TabIndex = 14;
+            label6.Text = "Categoria:";
+            label6.Click += label6_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 10F);
+            label5.Location = new Point(34, 153);
+            label5.Name = "label5";
+            label5.Size = new Size(41, 19);
+            label5.TabIndex = 13;
+            label5.Text = "ISBN:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10F);
+            label4.Location = new Point(34, 106);
+            label4.Name = "label4";
+            label4.Size = new Size(47, 19);
+            label4.TabIndex = 12;
+            label4.Text = "Autor:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10F);
+            label3.Location = new Point(34, 57);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 19);
+            label3.TabIndex = 11;
+            label3.Text = "Titulo:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10F);
+            label2.Location = new Point(34, 17);
+            label2.Name = "label2";
+            label2.Size = new Size(26, 19);
+            label2.TabIndex = 10;
+            label2.Text = "ID:";
+            // 
+            // textBoxEDispo
+            // 
+            textBoxEDispo.Location = new Point(200, 278);
+            textBoxEDispo.Name = "textBoxEDispo";
+            textBoxEDispo.Size = new Size(100, 23);
+            textBoxEDispo.TabIndex = 9;
+            // 
+            // textBoxETotal
+            // 
+            textBoxETotal.Location = new Point(200, 236);
+            textBoxETotal.Name = "textBoxETotal";
+            textBoxETotal.Size = new Size(100, 23);
+            textBoxETotal.TabIndex = 8;
+            // 
+            // textBoxCategoria
+            // 
+            textBoxCategoria.Location = new Point(200, 192);
+            textBoxCategoria.Name = "textBoxCategoria";
+            textBoxCategoria.Size = new Size(100, 23);
+            textBoxCategoria.TabIndex = 7;
+            // 
+            // textBoxISBN
+            // 
+            textBoxISBN.Location = new Point(200, 145);
+            textBoxISBN.Name = "textBoxISBN";
+            textBoxISBN.Size = new Size(100, 23);
+            textBoxISBN.TabIndex = 6;
+            // 
+            // textBoxAutor
+            // 
+            textBoxAutor.Location = new Point(200, 98);
+            textBoxAutor.Name = "textBoxAutor";
+            textBoxAutor.Size = new Size(100, 23);
+            textBoxAutor.TabIndex = 5;
+            // 
+            // textBoxTitulo
+            // 
+            textBoxTitulo.Location = new Point(200, 49);
+            textBoxTitulo.Name = "textBoxTitulo";
+            textBoxTitulo.Size = new Size(100, 23);
+            textBoxTitulo.TabIndex = 4;
+            // 
+            // textBoxid
+            // 
+            textBoxid.Location = new Point(200, 9);
+            textBoxid.Name = "textBoxid";
+            textBoxid.Size = new Size(100, 23);
+            textBoxid.TabIndex = 3;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(394, 57);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(115, 23);
+            dateTimePicker1.TabIndex = 2;
+            // 
+            // editar
+            // 
+            editar.Font = new Font("Segoe UI", 10F);
+            editar.Location = new Point(521, 330);
+            editar.Name = "editar";
+            editar.Size = new Size(93, 23);
+            editar.TabIndex = 1;
+            editar.Text = "Editar Datos";
+            editar.UseVisualStyleBackColor = true;
+            // 
+            // salir
+            // 
+            salir.Font = new Font("Segoe UI", 10F);
+            salir.Location = new Point(394, 330);
+            salir.Name = "salir";
+            salir.Size = new Size(75, 23);
+            salir.TabIndex = 0;
+            salir.Text = "Salir";
+            salir.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(12, 41, 64);
+            panel3.Location = new Point(-1, 1);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(112, 75);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(92, 109, 152);
+            panel4.Controls.Add(igualejemplar);
+            panel4.Controls.Add(label1);
+            panel4.Location = new Point(110, 1);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(691, 75);
+            panel4.TabIndex = 3;
+            // 
+            // igualejemplar
+            // 
+            igualejemplar.AutoSize = true;
+            igualejemplar.Font = new Font("Segoe UI", 18F);
+            igualejemplar.Location = new Point(124, 29);
+            igualejemplar.Name = "igualejemplar";
+            igualejemplar.Size = new Size(0, 32);
+            igualejemplar.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(3, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(127, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Ejemplar:";
+            // 
+            // FrmConsultaLibros
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "FrmConsultaLibros";
+            Text = "FrmEditConsultaLibros";
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private DateTimePicker dateTimePicker1;
+        private Button editar;
+        private Button salir;
+        private Label label1;
+        private TextBox textBoxEDispo;
+        private TextBox textBoxETotal;
+        private TextBox textBoxCategoria;
+        private TextBox textBoxISBN;
+        private TextBox textBoxAutor;
+        private TextBox textBoxTitulo;
+        private TextBox textBoxid;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label igualejemplar;
+    }
+}
