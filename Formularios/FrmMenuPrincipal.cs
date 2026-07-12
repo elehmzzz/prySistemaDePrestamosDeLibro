c1b4e8b81cb4fd54a729bf8808c624b8f0e8daaa
elehmzzz <elehmzzz@gmail.com>
2026-07-12 12:05:28 -0600

    Añadidos formularios de registro de usuario y datos
    Se han implementado los formularios `FrmRegistroUsuario` y
    `FrmRegistroDatos` con sus respectivos diseños y funcionalidades
    básicas.
    
    - `FrmRegistroUsuario`: permite ingresar un nombre de usuario y
      contraseña. Incluye botones para avanzar
      y regresar.
    - `FrmRegistroDatos`: permite ingresar datos personales como
      nombre, apellidos, fecha de nacimiento, correo, teléfono, entre
      otros. También incluye botones para avanzar y regresar.
    - Se incluyó un diseño adicional para `FrmRegistroPersonal` con
      un panel lateral y un área de contenido principal.

---
+++ Formularios/FrmMenuPrincipal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.DirectoryServices.ActiveDirectory;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prySistemaDePrestamosDeLibro.Formularios
+{
+    public partial class FrmMenuPrincipal : Form
+    {
+        private FrmRegistroDatos ventanaDatos;
+        private FrmRegistroUsuario ventanaUsuario;
+        public FrmMenuPrincipal()
+        {
+            InitializeComponent();
+            ventanaDatos = new FrmRegistroDatos();
+            ventanaUsuario = new FrmRegistroUsuario();
+            abrirFormulario(ventanaDatos);
+        }
+
+        public void mostrarFormularioDatos() {
+            abrirFormulario(ventanaDatos);
+        }
+
+        public void mostrarFormularioUsuario() {
+            abrirFormulario(ventanaUsuario);
+        }
+
+
+        private void abrirFormulario(Form formulario) {
+            //limpia el contenido del panel
+            pnlContenido.Controls.Clear();
+            formulario.TopLevel = false;
+            //hace que el formulario abarque todo el panel
+            formulario.Dock = DockStyle.Fill;
+            pnlContenido.Controls.Add(formulario);
+            formulario.Show();
+        }
+
+        
+
+    }
+}
