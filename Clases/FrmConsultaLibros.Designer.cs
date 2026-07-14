2070982c7cb6af3a7bc2d004a4c11337c21297f3
Hermelinda.Cruz <20251066@uthh.edu.mx>
2026-07-14 15:00:23 -0500

    Se modifico frmNuevoLibro,FrmEditConsultaLibro y FrmConsultaLibros

--- Clases/FrmConsultaLibros.Designer.cs
+++ Clases/FrmConsultaLibros.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
             panel2 = new Panel();
             label9 = new Label();
             label8 = new Label();
@@ -48,86 +47,63 @@
             dateTimePicker1 = new DateTimePicker();
             editar = new Button();
             salir = new Button();
-            panel3 = new Panel();
-            panel4 = new Panel();
-            igualejemplar = new Label();
             label1 = new Label();
+            label10 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel2 = new TableLayoutPanel();
             panel2.SuspendLayout();
-            panel4.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
             SuspendLayout();
-            // 
-            // panel1
-            // 
-            panel1.BackColor = Color.FromArgb(32, 41, 64);
-            panel1.Location = new Point(1, 74);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(110, 391);
-            panel1.TabIndex = 0;
             // 
             // panel2
             // 
-            panel2.BackColor = Color.FromArgb(236, 223, 204);
-            panel2.Controls.Add(label9);
-            panel2.Controls.Add(label8);
-            panel2.Controls.Add(label7);
-            panel2.Controls.Add(label6);
-            panel2.Controls.Add(label5);
-            panel2.Controls.Add(label4);
-            panel2.Controls.Add(label3);
-            panel2.Controls.Add(label2);
-            panel2.Controls.Add(textBoxEDispo);
-            panel2.Controls.Add(textBoxETotal);
-            panel2.Controls.Add(textBoxCategoria);
-            panel2.Controls.Add(textBoxISBN);
-            panel2.Controls.Add(textBoxAutor);
-            panel2.Controls.Add(textBoxTitulo);
-            panel2.Controls.Add(textBoxid);
-            panel2.Controls.Add(dateTimePicker1);
-            panel2.Controls.Add(editar);
-            panel2.Controls.Add(salir);
+            panel2.BackColor = SystemColors.Control;
+            panel2.Controls.Add(tableLayoutPanel2);
+            panel2.Controls.Add(tableLayoutPanel1);
             panel2.Font = new Font("Segoe UI", 9F);
-            panel2.Location = new Point(110, 74);
+            panel2.Location = new Point(-2, -2);
             panel2.Name = "panel2";
-            panel2.Size = new Size(691, 375);
+            panel2.Size = new Size(1038, 549);
             panel2.TabIndex = 1;
             // 
             // label9
             // 
             label9.AutoSize = true;
-            label9.Font = new Font("Segoe UI", 10F);
-            label9.Location = new Point(389, 17);
+            label9.Font = new Font("Segoe UI", 12F);
+            label9.Location = new Point(480, 0);
             label9.Name = "label9";
-            label9.Size = new Size(120, 19);
+            label9.Size = new Size(136, 21);
             label9.TabIndex = 17;
             label9.Text = "Fecha de Registro:";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Font = new Font("Segoe UI", 10F);
-            label8.Location = new Point(34, 286);
+            label8.Font = new Font("Segoe UI", 12F);
+            label8.Location = new Point(3, 317);
             label8.Name = "label8";
-            label8.Size = new Size(151, 19);
+            label8.Size = new Size(174, 21);
             label8.TabIndex = 16;
             label8.Text = "Ejemplares Disponibles:";
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Font = new Font("Segoe UI", 10F);
-            label7.Location = new Point(34, 244);
+            label7.Font = new Font("Segoe UI", 12F);
+            label7.Location = new Point(3, 265);
             label7.Name = "label7";
-            label7.Size = new Size(123, 19);
+            label7.Size = new Size(140, 21);
             label7.TabIndex = 15;
             label7.Text = "Ejemplares Totales:";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Font = new Font("Segoe UI", 10F);
-            label6.Location = new Point(34, 192);
+            label6.Font = new Font("Segoe UI", 12F);
+            label6.Location = new Point(3, 213);
             label6.Name = "label6";
-            label6.Size = new Size(71, 19);
+            label6.Size = new Size(80, 21);
             label6.TabIndex = 14;
             label6.Text = "Categoria:";
             label6.Click += label6_Click;
@@ -135,187 +111,222 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Font = new Font("Segoe UI", 10F);
-            label5.Location = new Point(34, 153);
+            label5.Font = new Font("Segoe UI", 12F);
+            label5.Location = new Point(3, 165);
             label5.Name = "label5";
-            label5.Size = new Size(41, 19);
+            label5.Size = new Size(47, 21);
             label5.TabIndex = 13;
             label5.Text = "ISBN:";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 10F);
-            label4.Location = new Point(34, 106);
+            label4.Font = new Font("Segoe UI", 12F);
+            label4.Location = new Point(3, 124);
             label4.Name = "label4";
-            label4.Size = new Size(47, 19);
+            label4.Size = new Size(52, 21);
             label4.TabIndex = 12;
             label4.Text = "Autor:";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 10F);
-            label3.Location = new Point(34, 57);
+            label3.Font = new Font("Segoe UI", 12F);
+            label3.Location = new Point(3, 62);
             label3.Name = "label3";
-            label3.Size = new Size(46, 19);
+            label3.Size = new Size(52, 21);
             label3.TabIndex = 11;
             label3.Text = "Titulo:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 10F);
-            label2.Location = new Point(34, 17);
+            label2.Font = new Font("Segoe UI", 12F);
+            label2.Location = new Point(3, 0);
             label2.Name = "label2";
-            label2.Size = new Size(26, 19);
+            label2.Size = new Size(28, 21);
             label2.TabIndex = 10;
             label2.Text = "ID:";
             // 
             // textBoxEDispo
             // 
-            textBoxEDispo.Location = new Point(200, 278);
+            textBoxEDispo.Location = new Point(194, 320);
             textBoxEDispo.Name = "textBoxEDispo";
-            textBoxEDispo.Size = new Size(100, 23);
+            textBoxEDispo.Size = new Size(232, 29);
             textBoxEDispo.TabIndex = 9;
             // 
             // textBoxETotal
             // 
-            textBoxETotal.Location = new Point(200, 236);
+            textBoxETotal.Location = new Point(194, 268);
             textBoxETotal.Name = "textBoxETotal";
-            textBoxETotal.Size = new Size(100, 23);
+            textBoxETotal.Size = new Size(232, 29);
             textBoxETotal.TabIndex = 8;
             // 
             // textBoxCategoria
             // 
-            textBoxCategoria.Location = new Point(200, 192);
+            textBoxCategoria.Location = new Point(194, 216);
             textBoxCategoria.Name = "textBoxCategoria";
-            textBoxCategoria.Size = new Size(100, 23);
+            textBoxCategoria.Size = new Size(232, 29);
             textBoxCategoria.TabIndex = 7;
             // 
             // textBoxISBN
             // 
-            textBoxISBN.Location = new Point(200, 145);
+            textBoxISBN.Location = new Point(194, 168);
             textBoxISBN.Name = "textBoxISBN";
-            textBoxISBN.Size = new Size(100, 23);
+            textBoxISBN.Size = new Size(232, 29);
             textBoxISBN.TabIndex = 6;
             // 
             // textBoxAutor
             // 
-            textBoxAutor.Location = new Point(200, 98);
+            textBoxAutor.Location = new Point(194, 127);
             textBoxAutor.Name = "textBoxAutor";
-            textBoxAutor.Size = new Size(100, 23);
+            textBoxAutor.Size = new Size(232, 29);
             textBoxAutor.TabIndex = 5;
             // 
             // textBoxTitulo
             // 
-            textBoxTitulo.Location = new Point(200, 49);
+            textBoxTitulo.Location = new Point(194, 65);
             textBoxTitulo.Name = "textBoxTitulo";
-            textBoxTitulo.Size = new Size(100, 23);
+            textBoxTitulo.Size = new Size(232, 29);
             textBoxTitulo.TabIndex = 4;
             // 
             // textBoxid
             // 
-            textBoxid.Location = new Point(200, 9);
+            textBoxid.Location = new Point(194, 3);
             textBoxid.Name = "textBoxid";
-            textBoxid.Size = new Size(100, 23);
+            textBoxid.Size = new Size(232, 29);
             textBoxid.TabIndex = 3;
             // 
             // dateTimePicker1
             // 
             dateTimePicker1.Format = DateTimePickerFormat.Short;
-            dateTimePicker1.Location = new Point(394, 57);
+            dateTimePicker1.Location = new Point(480, 65);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(115, 23);
+            dateTimePicker1.Size = new Size(190, 29);
             dateTimePicker1.TabIndex = 2;
             // 
             // editar
             // 
-            editar.Font = new Font("Segoe UI", 10F);
-            editar.Location = new Point(521, 330);
+            editar.BackColor = Color.FromArgb(32, 41, 64);
+            editar.Font = new Font("Segoe UI", 12F);
+            editar.ForeColor = Color.White;
+            editar.Location = new Point(676, 320);
             editar.Name = "editar";
-            editar.Size = new Size(93, 23);
+            editar.Size = new Size(133, 44);
             editar.TabIndex = 1;
             editar.Text = "Editar Datos";
-            editar.UseVisualStyleBackColor = true;
+            editar.UseVisualStyleBackColor = false;
             // 
             // salir
             // 
-            salir.Font = new Font("Segoe UI", 10F);
-            salir.Location = new Point(394, 330);
+            salir.BackColor = Color.FromArgb(32, 41, 64);
+            salir.Font = new Font("Segoe UI", 12F);
+            salir.ForeColor = Color.White;
+            salir.Location = new Point(480, 320);
             salir.Name = "salir";
-            salir.Size = new Size(75, 23);
+            salir.Size = new Size(133, 44);
             salir.TabIndex = 0;
             salir.Text = "Salir";
-            salir.UseVisualStyleBackColor = true;
-            // 
-            // panel3
-            // 
-            panel3.BackColor = Color.FromArgb(12, 41, 64);
-            panel3.Location = new Point(-1, 1);
-            panel3.Name = "panel3";
-            panel3.Size = new Size(112, 75);
-            panel3.TabIndex = 2;
-            // 
-            // panel4
-            // 
-            panel4.BackColor = Color.FromArgb(92, 109, 152);
-            panel4.Controls.Add(igualejemplar);
-            panel4.Controls.Add(label1);
-            panel4.Location = new Point(110, 1);
-            panel4.Name = "panel4";
-            panel4.Size = new Size(691, 75);
-            panel4.TabIndex = 3;
-            // 
-            // igualejemplar
-            // 
-            igualejemplar.AutoSize = true;
-            igualejemplar.Font = new Font("Segoe UI", 18F);
-            igualejemplar.Location = new Point(124, 29);
-            igualejemplar.Name = "igualejemplar";
-            igualejemplar.Size = new Size(0, 32);
-            igualejemplar.TabIndex = 1;
+            salir.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 20F);
-            label1.Location = new Point(3, 24);
+            label1.Location = new Point(3, 0);
             label1.Name = "label1";
             label1.Size = new Size(127, 37);
             label1.TabIndex = 0;
             label1.Text = "Ejemplar:";
             // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 20F);
+            label10.Location = new Point(202, 0);
+            label10.Name = "label10";
+            label10.Size = new Size(105, 37);
+            label10.TabIndex = 2;
+            label10.Text = "label10";
+            label10.Click += label10_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 4;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 40.0419273F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 59.9580727F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 196F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 147F));
+            tableLayoutPanel1.Controls.Add(textBoxid, 1, 0);
+            tableLayoutPanel1.Controls.Add(textBoxTitulo, 1, 1);
+            tableLayoutPanel1.Controls.Add(dateTimePicker1, 2, 1);
+            tableLayoutPanel1.Controls.Add(textBoxAutor, 1, 2);
+            tableLayoutPanel1.Controls.Add(label8, 0, 6);
+            tableLayoutPanel1.Controls.Add(editar, 3, 6);
+            tableLayoutPanel1.Controls.Add(textBoxISBN, 1, 3);
+            tableLayoutPanel1.Controls.Add(salir, 2, 6);
+            tableLayoutPanel1.Controls.Add(label7, 0, 5);
+            tableLayoutPanel1.Controls.Add(textBoxCategoria, 1, 4);
+            tableLayoutPanel1.Controls.Add(label6, 0, 4);
+            tableLayoutPanel1.Controls.Add(textBoxETotal, 1, 5);
+            tableLayoutPanel1.Controls.Add(label5, 0, 3);
+            tableLayoutPanel1.Controls.Add(textBoxEDispo, 1, 6);
+            tableLayoutPanel1.Controls.Add(label4, 0, 2);
+            tableLayoutPanel1.Controls.Add(label3, 0, 1);
+            tableLayoutPanel1.Controls.Add(label9, 2, 0);
+            tableLayoutPanel1.Controls.Add(label2, 0, 0);
+            tableLayoutPanel1.Font = new Font("Segoe UI", 12F);
+            tableLayoutPanel1.Location = new Point(29, 131);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 7;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 41F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 48F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 52F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 52F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            tableLayoutPanel1.Size = new Size(821, 367);
+            tableLayoutPanel1.TabIndex = 18;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Controls.Add(label10, 1, 0);
+            tableLayoutPanel2.Controls.Add(label1, 0, 0);
+            tableLayoutPanel2.Location = new Point(29, 35);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(399, 47);
+            tableLayoutPanel2.TabIndex = 19;
+            // 
             // FrmConsultaLibros
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(panel4);
-            Controls.Add(panel3);
+            ClientSize = new Size(1036, 544);
             Controls.Add(panel2);
-            Controls.Add(panel1);
-            FormBorderStyle = FormBorderStyle.FixedSingle;
+            FormBorderStyle = FormBorderStyle.None;
             MaximizeBox = false;
             Name = "FrmConsultaLibros";
             Text = "FrmEditConsultaLibros";
             panel2.ResumeLayout(false);
-            panel2.PerformLayout();
-            panel4.ResumeLayout(false);
-            panel4.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Panel panel1;
         private Panel panel2;
-        private Panel panel3;
-        private Panel panel4;
         private DateTimePicker dateTimePicker1;
         private Button editar;
         private Button salir;
-        private Label label1;
         private TextBox textBoxEDispo;
         private TextBox textBoxETotal;
         private TextBox textBoxCategoria;
@@ -331,6 +342,9 @@
         private Label label9;
         private Label label8;
         private Label label7;
-        private Label igualejemplar;
+        private Label label10;
+        private Label label1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
     }
 }
--- Clases/FrmConsultaLibros.cs
+++ Clases/FrmConsultaLibros.cs
@@ -21,5 +21,10 @@ namespace prySistemaDePrestamosDeLibro.Clases
         {
 
         }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
