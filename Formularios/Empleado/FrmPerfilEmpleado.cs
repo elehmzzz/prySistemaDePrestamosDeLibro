4cb09ec88b4e49ad2d04cb9ca61c6508203c2c2e
Hermelinda.Cruz <20251066@uthh.edu.mx>
2026-07-21 02:01:18 -0500

    se modifico el perfil empleados

--- Formularios/Empleado/FrmPerfilEmpleado.cs
+++ Formularios/Empleado/FrmPerfilEmpleado.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.Mozilla;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,59 @@ namespace prySistemaDePrestamosDeLibro.Clases
 {
     public partial class FrmPerfilEmpleado : Form
     {
-        public FrmPerfilEmpleado()
+
+        public FrmPerfilEmpleado(
+
+            string NombreCompleto,
+            string Usuario,
+            string Correo,
+            string Telefono)
+
         {
             InitializeComponent();
+            textBox1.Text = NombreCompleto;
+            textBox4.Text = Usuario;
+            textBox2.Text = Correo;
+            textBox3.Text = Telefono;
+
+        }
+
+
+        public void mostrarDatos()
+        {
+
+        }
+        private void FrmPerfilEmpleado_Load(object sender, EventArgs e)
+        {
+            mostrarDatos();
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmPerfilEmpleado_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmPerfilEmpleado_Load_2(object sender, EventArgs e)
+        {
+
         }
     }
 }
