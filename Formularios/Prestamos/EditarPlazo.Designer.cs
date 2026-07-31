a924fc89c545db29cd543db01905312aa7a16cc3
alexaviteramirez-bit <alexaviteramirez@gmail.com>
2026-07-30 22:21:49 -0600

    ajuste en diseño editar plazo

--- Formularios/Prestamos/EditarPlazo.Designer.cs
+++ Formularios/Prestamos/EditarPlazo.Designer.cs
@@ -28,72 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel1 = new Panel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            dateTimePicker1 = new DateTimePicker();
+            label1 = new Label();
             tableLayoutPanel1 = new TableLayoutPanel();
             btnCerrar = new Button();
             button2 = new Button();
-            label1 = new Label();
-            tableLayoutPanel2 = new TableLayoutPanel();
-            dateTimePicker1 = new DateTimePicker();
-            panel1.SuspendLayout();
-            tableLayoutPanel1.SuspendLayout();
             tableLayoutPanel2.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
-            // 
-            // panel1
-            // 
-            panel1.Controls.Add(tableLayoutPanel2);
-            panel1.Controls.Add(tableLayoutPanel1);
-            panel1.Dock = DockStyle.Fill;
-            panel1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            panel1.Location = new Point(0, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(445, 207);
-            panel1.TabIndex = 1;
-            // 
-            // tableLayoutPanel1
-            // 
-            tableLayoutPanel1.ColumnCount = 2;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.Controls.Add(btnCerrar, 0, 0);
-            tableLayoutPanel1.Controls.Add(button2, 1, 0);
-            tableLayoutPanel1.Location = new Point(210, 95);
-            tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 1;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.Size = new Size(217, 65);
-            tableLayoutPanel1.TabIndex = 0;
-            // 
-            // btnCerrar
-            // 
-            btnCerrar.AutoSize = true;
-            btnCerrar.Location = new Point(3, 3);
-            btnCerrar.Name = "btnCerrar";
-            btnCerrar.Size = new Size(96, 38);
-            btnCerrar.TabIndex = 0;
-            btnCerrar.Text = "Cancelar";
-            btnCerrar.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.AutoSize = true;
-            button2.Location = new Point(111, 3);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 38);
-            button2.TabIndex = 1;
-            button2.Text = "Guardar";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(3, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(181, 28);
-            label1.TabIndex = 1;
-            label1.Text = "Fecha de prestamo:";
-         
             // 
             // tableLayoutPanel2
             // 
@@ -102,7 +45,7 @@
             tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 53.5980148F));
             tableLayoutPanel2.Controls.Add(dateTimePicker1, 1, 0);
             tableLayoutPanel2.Controls.Add(label1, 0, 0);
-            tableLayoutPanel2.Location = new Point(12, 12);
+            tableLayoutPanel2.Location = new Point(12, 42);
             tableLayoutPanel2.Name = "tableLayoutPanel2";
             tableLayoutPanel2.RowCount = 1;
             tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
@@ -114,8 +57,56 @@
             dateTimePicker1.Format = DateTimePickerFormat.Short;
             dateTimePicker1.Location = new Point(190, 3);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(210, 34);
+            dateTimePicker1.Size = new Size(210, 27);
             dateTimePicker1.TabIndex = 32;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(138, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Fecha de prestamo:";
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(btnCerrar, 0, 0);
+            tableLayoutPanel1.Controls.Add(button2, 1, 0);
+            tableLayoutPanel1.Location = new Point(202, 131);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(231, 46);
+            tableLayoutPanel1.TabIndex = 0;
+            tableLayoutPanel1.Paint += tableLayoutPanel1_Paint_2;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.AutoSize = true;
+            btnCerrar.BackColor = Color.FromArgb(32, 41, 64);
+            btnCerrar.ForeColor = SystemColors.ButtonHighlight;
+            btnCerrar.Location = new Point(3, 3);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(96, 38);
+            btnCerrar.TabIndex = 0;
+            btnCerrar.Text = "Cancelar";
+            btnCerrar.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.AutoSize = true;
+            button2.BackColor = Color.FromArgb(32, 41, 64);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(118, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 38);
+            button2.TabIndex = 1;
+            button2.Text = "Guardar";
+            button2.UseVisualStyleBackColor = false;
             // 
             // EditarPlazo
             // 
@@ -123,22 +114,20 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(236, 223, 204);
             ClientSize = new Size(445, 207);
-            Controls.Add(panel1);
-            FormBorderStyle = FormBorderStyle.None;
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(tableLayoutPanel2);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
             MaximizeBox = false;
             Name = "EditarPlazo";
             Text = "FrmRPrestamo";
-            panel1.ResumeLayout(false);
-            tableLayoutPanel1.ResumeLayout(false);
-            tableLayoutPanel1.PerformLayout();
             tableLayoutPanel2.ResumeLayout(false);
             tableLayoutPanel2.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Panel panel1;
         private TableLayoutPanel tableLayoutPanel1;
         private Button btnCerrar;
         private Button button2;
