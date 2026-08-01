d06c2f6855ceb8f3aab53b0271770348582e03e1
alexaviteramirez-bit <alexaviteramirez@gmail.com>
2026-08-01 08:41:08 -0600

    modificaciones en multas

---
+++ Clases/ClsMulta.cs
@@ -0,0 +1,70 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prySistemaDePrestamosDeLibro.Clases
+{
+    internal class ClsMulta
+    {
+        private int IdMulta;
+        private decimal Penalizacion;
+        private int Dias_tolerancia;
+
+        //get y set
+        public int getIdMulta()
+        {
+            return IdMulta;
+        }
+
+        public void setIdMulta(int id)
+        {
+            IdMulta = id;
+        }
+
+        public decimal getPenalizacion()
+        {
+            return Penalizacion;
+        }
+
+        public void setPenalizacion(decimal penalizacion)
+        {
+            Penalizacion = penalizacion;
+        }
+
+        public int getDias_tolerancia()
+        {
+            return Dias_tolerancia;
+        }
+
+        public void setDias_tolerancia(int diast)
+        {
+            Dias_tolerancia = diast;
+        }
+
+        public DataRow ObtenerMultaPorId(int id)
+        {
+            DataTable dt = new DataTable();
+            ClsConexion conexion = new ClsConexion();
+            MySqlConnection con = conexion.ObtenerConexion();
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("SELECT Id_Multa, Penalizacion, Dias_tolerancia FROM multa WHERE Id_Multa = @id", con);
+                cmd.Parameters.AddWithValue("@id", id);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                adapter.Fill(dt);
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener la multa: " + ex.Message);
+            }
+
+            return dt.Rows.Count > 0 ? dt.Rows[0] : null;
+        }
+    }
+}
--- Formularios/Multas/FrmMultas.cs
+++ Formularios/Multas/FrmMultas.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using prySistemaDePrestamosDeLibro.Clases;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,13 +13,28 @@ namespace prySistemaDePrestamosDeLibro.Formularios.Multas
 {
     public partial class FrmMultas : Form
     {
+        ClsMulta objMulta = new ClsMulta();
+
         public FrmMultas()
         {
             InitializeComponent();
+            objMulta=new ClsMulta();
         }
-
+         
         private void txtDiasTolerancia_TextChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void frmLoadMultas(object sender, EventArgs e)
+        {
+            DataRow fila = objMulta.ObtenerMultaPorId(2);
+
+            if (fila != null)
+            {
+                txtDiasTolerancia.Text = fila["Dias_tolerancia"].ToString();
+                txtPenalizacion.Text = fila["Penalizacion"].ToString();
+            }
 
         }
     }
